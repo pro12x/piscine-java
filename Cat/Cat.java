b7e734ce9afba8f4ed1e1b91bf536b8d8d1f9fc0
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2025-03-11 18:37:56 +0100

    Cat pushed at Tue 11 Mar 18:37:56 WAT 2025

--- Cat/Cat.java
+++ Cat/Cat.java
@@ -6,7 +6,7 @@ public class Cat {
     public static void cat(String[] args) throws IOException {
         for (String filename : args) {
             try (InputStream inputStream = new FileInputStream(filename)) {
-                byte[] buffer = new byte[4096];
+                byte[] buffer = new byte[2048];
                 int bytesRead;
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     System.out.write(buffer, 0, bytesRead);
