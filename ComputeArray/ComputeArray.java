c9258e5f3517127885d50ee48f55329ece10ad56
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2025-03-11 12:02:58 +0100

    ComputeArray pushed at Tue 11 Mar 12:02:58 WAT 2025

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,18 @@
+public class ComputeArray {
+    public static int[] computeArray(int[] array) {
+        int[] result = new int[array.length];
+
+        int i = 0;
+        for (int value : array) {
+            if (value % 3 == 0) {
+                result[i] = value * 5;
+            } else if (value % 3 == 1) {
+                result[i] = value + 7;
+            } else {
+                result[i] = value;
+            }
+            i++;
+        }
+        return result;
+    }
+}
--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -1,5 +1,5 @@
 public class Palindrome {
     public static boolean isPalindrome(String s) {
-        return s.contentEquals(new StringBuilder(s).reverse());
+        return s.contentEquals(new StringBuilder(s).reverse()) && !s.trim().isEmpty();
     }
 }
