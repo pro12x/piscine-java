92aeecca08f2dcd24db1312dc78f5917a1295d5c
Franchis Janel MOKOMBA <janelmokomba@gmail.com>
2025-03-11 13:39:56 +0100

    DoOp pushed at Tue 11 Mar 13:39:56 WAT 2025

--- DoOp/DoOp.java
+++ DoOp/DoOp.java
@@ -16,11 +16,4 @@ public class DoOp {
         }
         return "it depend on the input";
     }
-
-    public static void main(String[] args) {
-        System.out.println(DoOp.operate(new String[]{"1","+","2"}));
-        System.out.println(DoOp.operate(new String[]{"1","-","1"}));
-        System.out.println(DoOp.operate(new String[]{"1","%","0"}));
-        System.out.println(DoOp.operate(args));
-    }
 }
